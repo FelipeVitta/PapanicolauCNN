872440cfa76a8c42564ad19838a6770e799436bd
FelipeVitta <feliperiku@gmail.com>
2023-12-03 22:31:10 -0300

    adjustment convolutional

--- scripts/Convolutional_binary.py
+++ scripts/Convolutional_binary.py
@@ -1,14 +1,17 @@
 from tensorflow.keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.utils import class_weight
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras import models, layers, optimizers
+from tensorflow.keras.models import load_model
 import numpy as np
 import os
 
 input_folder = './output_cell_images'
-save_directory = './models_trained/Convolutional_categorical'
+save_directory = './models_trained/Convolutional_binary'
+model_path = os.path.join(save_directory, 'efficientnet_model.h5')
 
 class_mapping = {
     'ASC-H': 'Positive for intraepithelial lesion',
@@ -21,6 +24,26 @@ class_mapping = {
 
 dataset = []
 labels = []
+predicted_classes = []
+img_size = (100, 100)
+
+# Função para preparar uma imagem para classificação
+def prepare_image(file_path, img_size):
+    img = load_img(file_path, target_size=img_size)
+    img_array = img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.  # Normalizando os pixels
+
+    return img_array
+
+# Função para fazer a previsão
+def classify_image(file_path, model, img_size, labels):
+    img_ready = prepare_image(file_path, img_size)
+    predictions = model.predict(img_ready)
+    predicted_class = np.argmax(predictions, axis=1)
+    predicted_label = labels[predicted_class[0]]
+
+    return predicted_label
 
 for folder_name in os.listdir(input_folder):
     if folder_name in class_mapping:
@@ -29,18 +52,15 @@ for folder_name in os.listdir(input_folder):
 
         for image_name in os.listdir(folder_path):
             image_path = os.path.join(folder_path, image_name)
-            image = load_img(image_path, target_size=(100, 100))
+            image = load_img(image_path, target_size=img_size)
             image = img_to_array(image)
+            image /= 255.0  # Normalizando os pixels
 
             dataset.append(image)
             labels.append(class_label)
 
-
 dataset = np.array(dataset)
-dataset = dataset / 255.0
-
-le = LabelEncoder()
-labels_encoded = le.fit_transform(labels)
+labels_encoded = LabelEncoder().fit_transform(labels)
 labels_categorical = to_categorical(labels_encoded)
 
 X_train, X_val, y_train, y_val = train_test_split(dataset, labels_categorical, test_size=0.2, random_state=42)
@@ -50,29 +70,56 @@ train_datagen = ImageDataGenerator(
     zoom_range=0.2,
     horizontal_flip=True
 )
+
 val_datagen = ImageDataGenerator()
 
 train_generator = train_datagen.flow(X_train, y_train, batch_size=32)
 val_generator = val_datagen.flow(X_val, y_val, batch_size=32)
 
-# Modelo EfficientNet
-base_model = EfficientNetB0(include_top=False, weights='imagenet', input_shape=(100, 100, 3))
-
-model = models.Sequential()
-model.add(base_model)
-model.add(layers.GlobalAveragePooling2D())
-model.add(layers.Dense(128, activation='relu'))
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(len(np.unique(labels_encoded)), activation='softmax'))
-
-model.compile(optimizer=optimizers.Adam(learning_rate=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
-
-# Treinando o modelo
-epochs = 1
-history = model.fit(
-    train_generator,
-    epochs=epochs,
-    validation_data=val_generator
+# Calcula os pesos das classes de forma que classes com menos amostras tenham um peso maior
+class_weights = class_weight.compute_class_weight(
+    class_weight='balanced',
+    classes=np.unique(labels_encoded),
+    y=labels_encoded
 )
 
-model.save('efficientnet_model.h5')
+class_weights_dict = dict(enumerate(class_weights))
+
+if os.path.exists(model_path):
+    print("Modelo carregado com sucesso.")
+    model = load_model(model_path)
+    diretorio_imagens = './image_nucleus'
+     # Percorrer todas as imagens no diretório
+    for nome_arquivo in os.listdir(diretorio_imagens):
+        # Montar o caminho completo da imagem
+        img_path = os.path.join(diretorio_imagens, nome_arquivo)
+        # Classificar a imagem
+        predicted_label = classify_image(img_path, model, img_size, labels)
+        print(f"Classe prevista para {nome_arquivo}: {predicted_label}")
+        predicted_classes.append(predicted_label)
+        
+else:  
+    print('Modelo não encontrado. Criando um novo modelo...')
+    base_model = EfficientNetB0(include_top=False, weights='imagenet', input_shape=(100, 100, 3))
+
+    model = models.Sequential()
+    model.add(base_model)
+    model.add(layers.GlobalAveragePooling2D())
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(len(np.unique(labels_encoded)), activation='softmax'))
+
+    model.compile(optimizer=optimizers.Adam(learning_rate=1e-4), 
+                  loss='categorical_crossentropy', 
+                  metrics=['accuracy'],
+                  class_weight=class_weights_dict)
+
+    # Treinando o modelo
+    epochs = 5
+    history = model.fit(
+        train_generator,
+        epochs=epochs,
+        validation_data=val_generator
+    )
+
+    model.save(model_path)
--- scripts/Convolutional_categorical.py
+++ scripts/Convolutional_categorical.py
@@ -1,14 +1,38 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 from tensorflow.keras import models, layers, optimizers
+from tensorflow.keras import regularizers
+import numpy as np
 import os
 
 input_folder = './output_cell_images'
 save_directory = './models_trained/Convolutional_categorical'
+model_path = os.path.join(save_directory, 'efficientnet_model.h5')
+
+predicted_classes = []
+
+# Função para preparar uma imagem para classificação
+def prepare_image(file_path, img_size):
+    img = image.load_img(file_path, target_size=img_size)
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.  # Normalizando os pixels
+
+    return img_array
+
+# Função para fazer a previsão
+def classify_image(file_path, model, img_size, labels):
+    img_ready = prepare_image(file_path, img_size)
+    predictions = model.predict(img_ready)
+    predicted_class = np.argmax(predictions, axis=1)
+    predicted_label = labels[predicted_class[0]]
+
+    return predicted_label
 
 labels = os.listdir(input_folder)
-print(labels)
 
 datagen = ImageDataGenerator(
     shear_range=0.2,
@@ -38,24 +62,44 @@ val_generator = datagen.flow_from_directory(
     classes=labels
 )
 
-# EfficientNet
-base_model = EfficientNetB0(include_top=False, weights='imagenet', input_shape=(100, 100, 3))
+# Carregar modelo se existir, caso contrário, criar um novo
+if os.path.exists(model_path):
+    print("Modelo carregado com sucesso.")
+    model = load_model(model_path)
+    diretorio_imagens = './image_nucleus'
+     # Percorrer todas as imagens no diretório
+    for nome_arquivo in os.listdir(diretorio_imagens):
+        # Montar o caminho completo da imagem
+        img_path = os.path.join(diretorio_imagens, nome_arquivo)
+        # Classificar a imagem
+        predicted_label = classify_image(img_path, model, img_size, labels)
+        print(f"Classe prevista para {nome_arquivo}: {predicted_label}")
+        predicted_classes.append(predicted_label)
+        
+    #print(predicted_classes)
+else:
+    print("Modelo não encontrado. Criando um novo modelo.")
+    base_model = EfficientNetB0(include_top=False, weights='imagenet', input_shape=(100, 100, 3))
 
-model = models.Sequential()
-model.add(base_model)
-model.add(layers.GlobalAveragePooling2D())
-model.add(layers.Dense(128, activation='relu'))
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(len(labels), activation='softmax'))
+    model = models.Sequential()
+    model.add(base_model)
+    model.add(layers.Dense(128, activation='relu', kernel_regularizer=regularizers.l1(0.01)))
+    model.add(layers.Dense(128, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+    model.add(layers.GlobalAveragePooling2D())
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(len(labels), activation='softmax'))
 
-model.compile(optimizer=optimizers.Adam(learning_rate=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=optimizers.Adam(learning_rate=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Treina o modelo
-epochs = 40
-history = model.fit(
-    train_generator,
-    epochs=epochs,
-    validation_data=val_generator
-)
+    # Treina o modelo
+    epochs = 7
+    history = model.fit(
+        train_generator,
+        epochs=epochs,
+        validation_data=val_generator
+    )
 
-model.save('efficientnet_model.h5')
+    model.save(model_path)
+
+
